657d9f409edcd88c25342318c07c194714dbe33f
Youssef <youssefelouamari73@gmail.com>
2020-09-06 15:28:47 +0200

    Maak een nieuw account

---
+++ signup.php
@@ -0,0 +1,13 @@
+<div class="new_account-form">
+  <h2>Maak een nieuw account</h2>
+  <form>
+    <input type="text" title="Voornaam" placeholder="Voornaam"/>
+    <input type="text" title="Tussenvoegsel" placeholder="Tussenvoegsel"/><br>
+    <input type="text" title="Achternaam" placeholder="Achternaam"/><br>
+    <input type="text" title="Email" placeholder="Email"/><br>
+    <input type="text" title="Gebruikersnaam" placeholder="Gebruikersnaam"/><br>
+    <input type="password" title="Wachtwoord" placeholder="Wachtwoord"/><br>
+    <input type="password" title="Herhaal_Wachtwoord" placeholder="Herhaal Wachtwoord"/><br>
+    <button type="submit" class="btn">Maak account!</button><br>
+  </form>
+</div>
