a7470d876ffdbd11549acea8ee7a5b3090d9617f
Youssef <youssefelouamari73@gmail.com>
2020-09-06 15:36:35 +0200

    Inlog page

---
+++ index.php
@@ -0,0 +1,10 @@
+<div class="log-form">
+  <h2>Login</h2>
+  <form>
+    <input type="text" title="Gebruikersnaam" placeholder="Gebruikersnaam"/><br>
+    <input type="password" title="Wachtwoord" placeholder="Wachtwoord"/><br>
+    <button type="submit" class="btn">Login</button><br>
+    <a class="forgot" href="http://127.0.0.1/project1/lostpsw.php">Wachtwoord vergeten?</a><br>
+    <a class="forgot" href="http://127.0.0.1/project1/signup.php">Maak een nieuw account</a>
+  </form>
+</div>
