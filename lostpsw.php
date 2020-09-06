8a01380977d96608bddee9b4316fce705d079fc2
Youssef <youssefelouamari73@gmail.com>
2020-09-06 15:37:49 +0200

    wachtwoord vergeten

---
+++ lostpsw.php
@@ -0,0 +1,7 @@
+<div class="log-form">
+  <h2>Wachtwoord vergeten</h2>
+  <form>
+    <input type="text" title="Email" placeholder="Gebruikersnaam"/><br>
+    <button type="submit" class="btn">Request</button><br>
+  </form>
+</div>
